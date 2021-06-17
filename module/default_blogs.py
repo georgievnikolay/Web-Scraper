3a57bc74f71f90dfcb506d9986e6b418b6942e6c
Kiril Ivanov <ivanovkirilg@gmail.com>
2021-06-17 13:59:27 +0300

    fix default_blogs comment item names better message if main is run with invalid website

--- module/default_blogs.py
+++ module/default_blogs.py
@@ -14,7 +14,7 @@ def travelsmart():
 def bozho():
     bozho = WebScraper("https://blog.bozho.net/")
     bozho.add_items( Item('content', 'div', {'class' : 'post-content'}), 
-                     Item('content-author', 'div', {'class' : 'comment-author'}),
+                     Item('comment-author', 'div', {'class' : 'comment-author'}),
                      Item('comment-text', 'div', {'class': 'comment-content'}) 
                     )
     return bozho
@@ -23,7 +23,7 @@ def bozho():
 def pateshestvenik():
     pateshestvenik = WebScraper("https://pateshestvenik.com/")
     pateshestvenik.add_items( Item('content', 'div', {'class' : 'content'}), 
-                              Item('content-author', 'div', {'class' : 'comment-author'}),
+                              Item('comment-author', 'div', {'class' : 'comment-author'}),
                               Item('comment-text', 'div', {'class': 'comment-text'})
                             )
     return pateshestvenik
@@ -32,7 +32,7 @@ def pateshestvenik():
 def az_moga():
     az_moga = WebScraper("https://az-moga.com/")
     az_moga.add_items( Item('content', 'div', {'class' : 'entry-body'}), 
-                       Item('content-author', 'div', {'class' : 'comment-author'}),
+                       Item('comment-author', 'div', {'class' : 'comment-author'}),
                        Item('comment-text', 'div', {'class': 'comment-content'})
                      )
     return az_moga
@@ -41,7 +41,7 @@ def az_moga():
 def igicheva():
     igicheva = WebScraper("https://igicheva.wordpress.com/all-posts/")
     igicheva.add_items( Item('headline', 'h1', {'class': 'entry-title'}), 
-                        Item('content-author', 'div', {'class' : 'comment-author'}),
+                        Item('comment-author', 'div', {'class' : 'comment-author'}),
                         Item('comment-text', 'div', {'class': 'comment-content'})
                        )
     igicheva.set_article_search_item(Item('', 'article', {'class': 'type-post'}))
