8dbb0de7bc7820336dbd38d6cbbde363056cf23d
Kiril Ivanov <ivanovkirilg@gmail.com>
2021-06-23 21:12:08 +0300

    run.sh cmdline arg

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 PRIMARY_BLOG='travelsmart' 
 EXTRA_BLOGS=('bozho' 'az_moga' 'igicheva' 'pateshestvenik')
 
+# Make sure to terminate the server, even if we exit forcefully
 clean_up () {
     echo Terminating...
     pkill flask
@@ -11,9 +12,23 @@ clean_up () {
 
 trap clean_up SIGINT SIGTERM
 
+# Check for a valid argument - number of posts to scrape
+if [[ $1 ]]
+then
+    if [[ $1 =~ ^[0-9]+$ ]]
+    then
+        NUM_POSTS=$1
+    else
+        echo Invalid argument $1. Should be an integer.
+        exit 1
+    fi
+else
+    NUM_POSTS=20
+fi
+
 # Scrape some initial data
 echo Sraping $PRIMARY_BLOG...
-python3 main.py $PRIMARY_BLOG
+python3 main.py $PRIMARY_BLOG -n $NUM_POSTS
 
 # Start up the server and open the home page
 echo Starting server...
@@ -26,7 +41,7 @@ xdg-open http://127.0.0.1:5000/
 for SINGLE_BLOG in ${EXTRA_BLOGS[@]}
 do
     echo Sraping $SINGLE_BLOG...
-    python3 main.py $SINGLE_BLOG
+    python3 main.py $SINGLE_BLOG -n $NUM_POSTS
 done
 
 # Wait for the user to be done browsing
