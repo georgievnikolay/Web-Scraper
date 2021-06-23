424e5bfcb06649f82921820e01caa9b74924dc41
georgievnikolay <nikolay.georgievfx@gmail.com>
2021-06-23 20:37:35 +0300

    run.sh in progress
    Co-authored-by: Kiril Ivanov <ivanovkirilg@users.noreply.github.com>

--- run.sh
+++ run.sh
@@ -3,19 +3,34 @@
 PRIMARY_BLOG='travelsmart' 
 EXTRA_BLOGS=('bozho' 'az_moga' 'igicheva' 'pateshestvenik')
 
+clean_up () {
+    echo Terminating...
+    pkill flask
+    exit
+}
+
+trap clean_up SIGINT SIGTERM
+
+# Scrape some initial data
 echo Sraping $PRIMARY_BLOG...
 python3 main.py $PRIMARY_BLOG
 
-python3 web_instance.py >/dev/null 2>/dev/null &
+# Start up the server and open the home page
+echo Starting server...
+export FLASK_APP=web_instance.py
+flask run >/dev/null 2>/dev/null &
+sleep 1
 xdg-open http://127.0.0.1:5000/
 
+# Scrape the other blogs
 for SINGLE_BLOG in ${EXTRA_BLOGS[@]}
 do
     echo Sraping $SINGLE_BLOG...
     python3 main.py $SINGLE_BLOG
 done
 
+# Wait for the user to be done browsing
 echo "Press return to terminate server"
 read
 
-kill -9 $(lsof -ti tcp:5000)
+clean_up
--- web_instance.py
+++ web_instance.py
@@ -2,4 +2,4 @@ from os import pardir
 from web_app import app
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
